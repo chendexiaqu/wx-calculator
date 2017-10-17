96d22e9d9d6d581412aff464567c9d02936ded24
Rundong Lian <rundong.lian@thoughtworks.com>
2017-10-17 14:35:37 +0800

    Logic

--- pages/calc/calc.js
+++ pages/calc/calc.js
@@ -28,23 +28,23 @@ Page({
     logs:[]
   },
   onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+    
   },
   onReady:function(){
-    // 页面渲染完成
+   
   },
   onShow:function(){
-    // 页面显示
+    
   },
   onHide:function(){
-    // 页面隐藏
+    
   },
   onUnload:function(){
-    // 页面关闭
+    
   },
   clickBtn:function(event){
     var id = event.target.id;
-    if(id == this.data.idb){  //退格←
+    if(id == this.data.idb){
       var data = this.data.screenData;
       if(data == "0"){
           return;
@@ -55,10 +55,10 @@ Page({
       }
       this.setData({"screenData":data});
       this.data.arr.pop();
-    }else if(id == this.data.idc){  //清屏C
+    }else if(id == this.data.idc){ 
       this.setData({"screenData":"0"});
       this.data.arr.length = 0;
-    }else if(id == this.data.idt){  //正负号+/-
+    }else if(id == this.data.idt){
       var data = this.data.screenData;
       if(data == "0"){
           return;
@@ -72,14 +72,12 @@ Page({
         this.data.arr.unshift("－");
       }
       this.setData({"screenData":data});
-    }else if(id == this.data.ide){  //等于＝
+    }else if(id == this.data.ide){ 
       var data = this.data.screenData;
       if(data == "0"){
           return;
       }
-      //eval是js中window的一个方法，而微信页面的脚本逻辑在是在JsCore中运行，JsCore是一个没有窗口对象的环境，所以不能再脚本中使用window，也无法在脚本中操作组件                 
-      //var result = eval(newData);           
-      
+
       var lastWord = data.charAt(data.length);
       if(isNaN(lastWord)){
         return;
@@ -116,7 +114,7 @@ Page({
             }
         }
       }
-      //存储历史记录
+      
       this.data.logs.push(data + result);
       wx.setStorageSync("calclogs",this.data.logs);
 
@@ -125,7 +123,7 @@ Page({
 
       this.setData({"screenData":result+""});
     }else{
-      if(this.data.operaSymbo[id]){ //如果是符号+-*/
+      if(this.data.operaSymbo[id]){
         if(this.data.lastIsOperaSymbo || this.data.screenData == "0"){
           return;
         }
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,13 @@
-//index.js
-//获取应用实例
 var app = getApp()
 Page({
   data: {
-    motto: '简易计算器☞',
+    motto: 'Simple Calculator',
     userInfo: {},
     defaultSize: 'default',
     disabled: false,
     iconType:'info_circle'
   },
-  //事件处理函数
+  
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
@@ -23,9 +21,9 @@ Page({
   onLoad: function () {
     console.log('onLoad');
     var that = this
-  	//调用应用实例的方法获取全局数据
+  
     app.getUserInfo(function(userInfo){
-      //更新数据
+      
       that.setData({
         userInfo:userInfo
       })
